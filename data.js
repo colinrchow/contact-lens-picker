427e6631db87c5910a5ed2545c2497792aa490e9
Colin C. <chowtime2@gmail.com>
2026-03-04 18:31:43 -0800

    Added input buttons and header

--- data.js
+++ data.js
@@ -1,5 +1,69 @@
 export const contactLensOptions = [
     {
+        name: '',
+        link: '',
+        image: './images/.png',
+        vibe: [],
+        outfit: [],
+        isPrescripted: true,
+
+    },
+    {
+        name: '',
+        link: '',
+        image: './images/.png',
+        vibe: [],
+        outfit: [],
+        isPrescripted: true,
+
+    },
+    {
+        name: '',
+        link: '',
+        image: './images/.png',
+        vibe: [],
+        outfit: [],
+        isPrescripted: true,
+
+    },
+    {
+        name: '',
+        link: '',
+        image: './images/.png',
+        vibe: [],
+        outfit: [],
+        isPrescripted: true,
+
+    },
+    {
+        name: '',
+        link: '',
+        image: './images/.png',
+        vibe: [],
+        outfit: [],
+        isPrescripted: true,
+
+    },
+    {
+        name: '',
+        link: '',
+        image: './images/.png',
+        vibe: [],
+        outfit: [],
+        isPrescripted: true,
 
     },
 ]
+
+// vibe:
+// - soft 
+// - bold 
+// - natural
+
+// outfit:
+// - neutral
+// - warm tone 
+// - cool tone 
+// - earth tone 
+// - pinks 
+// - metallic
